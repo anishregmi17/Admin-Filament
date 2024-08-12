1920561669658231dc39a037cb5cf438fd6f7ccf
anishregmi17 <anishregminaglibang@gmail.com>
2024-08-12 15:10:48 +0545

    update crud for customers

--- database/migrations/2024_08_12_052815_create_payments_table.php
+++ database/migrations/2024_08_12_052815_create_payments_table.php
@@ -11,14 +11,9 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('customers', function (Blueprint $table) {
+        Schema::create('payments', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('phone');
-            $table->text('address');
             $table->timestamps();
-            $table->softDeletes();
         });
     }
 
@@ -27,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('customers');
+        Schema::dropIfExists('payments');
     }
 };
