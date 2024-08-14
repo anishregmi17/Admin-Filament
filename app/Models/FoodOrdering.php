dac0f4bdbc4db89f0c91c963c69b6370aa2ef846
anishregmi17 <anishregminaglibang@gmail.com>
2024-08-14 20:38:38 +0545

    IMP:foodorder model added

---
+++ app/Models/FoodOrdering.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class FoodOrdering extends Model
+{
+    use HasFactory;
+    use SoftDeletes;
+
+
+    protected $fillable = ['customer_id', 'food_item_id', 'quantity', 'status'];
+
+    public function customer()
+    {
+        return $this->belongsTo(Customer::class);
+    }
+
+    public function foodItem()
+    {
+        return $this->belongsTo(FoodItem::class);
+    }
+}
