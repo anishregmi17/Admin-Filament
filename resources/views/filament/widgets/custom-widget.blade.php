652b5aea7c9fb10e698d5b8b8248de2fa5890c3b
anishregmi17 <anishregminaglibang@gmail.com>
2024-08-17 18:20:55 +0545

    chart js added

---
+++ resources/views/filament/widgets/custom-widget.blade.php
@@ -0,0 +1,6 @@
+<x-filament::widget>
+    <x-filament::card>
+        <h2>Custom Widget</h2>
+        <p>This is a custom widget for your dashboard.</p>
+    </x-filament::card>
+</x-filament::widget>
