da22840eb641ee9e29bc4bbef71663c6302d419e
anishregmi17 <anishregminaglibang@gmail.com>
2024-10-04 19:54:18 +0545

    Stats overview widget name TestWidget added including all customer res staff food item and user no

---
+++ app/Filament/Widgets/TestWidget.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Filament\Widgets;
+
+use App\Models\Customer;
+use App\Models\User;
+use App\Models\FoodItem;
+use App\Models\RestaurantStaff;
+use Filament\Widgets\StatsOverviewWidget as BaseWidget;
+use Filament\Widgets\StatsOverviewWidget\Stat;
+
+class TestWidget extends BaseWidget
+{
+    protected function getStats(): array
+    {
+        return [
+            Stat::make('Total Customers', Customer::count())
+                ->description('Number of customers')
+                ->color('success'),
+
+            Stat::make('Total Users', User::count())
+                ->description('Registered users')
+                ->color('primary'),
+
+            Stat::make('Total Food Items', FoodItem::count())
+                ->description('Food items available')
+                ->color('info'),
+
+            Stat::make('Total Staff Members', RestaurantStaff::count())
+                ->description('Number of restaurant staff')
+                ->color('warning'),
+        ];
+    }
+}
